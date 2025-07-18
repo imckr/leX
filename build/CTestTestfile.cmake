64a21ddd7eb72044f5722ab9f9597f6bfda8ae5e
Cloud User <ec2-user@ip-172-31-5-145.ap-south-1.compute.internal>
2025-07-18 19:11:34 +0000

    build tracking stopped

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ec2-user/chandra/lex
-# Build directory: /home/ec2-user/chandra/lex/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ec2-user/chandra/lex/tests
-# Build directory: /home/ec2-user/chandra/lex/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
